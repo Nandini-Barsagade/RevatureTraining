ff439d544fb0032acd27463e23135192da2304f4
Nandini-Barsagade <135138587+Nandini-Barsagade@users.noreply.github.com>
2024-08-06 23:01:46 +0530

    Add files via upload

---
+++ 05-08-2024/CalculatorProject/src/main/java/CalculatorMain.java
@@ -0,0 +1,9 @@
+package main.java;
+
+public class CalculatorMain {
+public static void main(String[] args) {
+	Calculator calculator=new Calculator();
+	int result=calculator.sum(10, 20);
+	System.out.println("Sum is :: "+result);
+}
+}
